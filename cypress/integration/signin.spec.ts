7212c67dbab47b57396b0e721437978df18f455c
Sam Palmer <github@sampalmer.dev>
2019-07-31 17:41:27 +0100

    Added a failing test

--- cypress/integration/signin.spec.ts
+++ cypress/integration/signin.spec.ts
@@ -7,6 +7,11 @@ describe('TDR', () => {
     cy.get('a').should('contain', 'Sign in')
   })
 
+  it('should fail the test if the test is obviously wrong', () => {
+    cy.visit('/')
+    cy.get('a').should('contain', 'Sign ina')
+  })
+
   it('should display an error if you visit the dashboard page without signing in', () => {
     cy.request({
       url: '/dashboard',
